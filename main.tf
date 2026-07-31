603d3e4dc96250220081cbafd6e082b095c276e1
Hoit1210 <wlqnwkdl@DESKTOP-5AOETF4.localdomain>
2026-08-01 00:24:18 +0900

    Fix: Change security group name to avoid duplicate error

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 # 보안 그룹 설정 (웹, Grafana, Prometheus 포트 오픈)
 resource "aws_security_group" "sre_sg" {
-  name        = "sre-project-sg"
+  name        = "sre-project-sg-v2"
   description = "Allow HTTP and SRE ports"
 
   ingress {
